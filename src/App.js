06df45b29387b9d4bc1a61442838a2af84b34b98
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-16 17:41:44 +0900

    기본 폴더들 만들고, 이름 입력과 웰컴 문구

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { userState } from "react";
+import styled from "styled-components";
+function Username() {
+  return <input placeholder="이름을 입력하세요."></input>;
+}
+
+function ButtonComponent({ idx, type, text, onclick }) {
+  const onClickBtn = () => {
+    onclick(idx);
+  };
+
+  return (
+    <div type={type} onClick={onClickBtn}>
+      {text}
+    </div>
+  );
+}
+
+function CheckBox() {
+  return;
+}
 
 function App() {
   return (
     <div className="App">
       <header>MBTI 직업 심리 검사</header>
+      <body>
+        <Username></Username>
+        <ButtonComponent></ButtonComponent>
+      </body>
     </div>
   );
 }
