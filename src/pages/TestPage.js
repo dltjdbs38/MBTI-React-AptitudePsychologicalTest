180aa2238cd397b0344627e73640082c37d6dff2
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-18 12:45:05 +0900

    progress 수정, 5개씩 질문 받아오는 currentData 추가 그러나 오류있음.

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -5,11 +5,11 @@ import axios from "axios";
 
 function TestPage() {
   const [progress, setProgress] = useState(0);
-  const [saveData, setSaveData] = useState([]);
+  const [saveData, setSaveData] = useState([]); //length 28인 Array
   const [userAnswer, setUserAnswer] = useState();
   const [totalAnswer, setTotalAnswer] = useState([]);
   const [totalScore, setTotalScore] = useState(0);
-
+  const [currentData, setCurrentData] = useState([, , , ,]);
   useEffect(() => {
     //컴포넌트가 렌더링 될 떄마다 특정 작업을 실행할 수 있도록
     axios
@@ -20,17 +20,37 @@ function TestPage() {
         console.log(res);
         setSaveData(res.data.RESULT);
         setTotalAnswer(Array(saveData.length));
+        for (let i = 0; i <= saveData.length - 1; i++) {
+          for (let j = 0; j <= currentData.length - 1; j++) {
+            currentData[i] = saveData[i];
+          }
+        }
+        console.log(currentData);
       })
       .catch((err) => {
         console.log(err);
       }); //한번만 실행 원하면 ({함수},[]) 리렌더링시마다 실행하고싶으면 ({함수}) 특정 state가 바뀔떄마다 실행 ({함수},[name])
   }, []);
+  function PlusProgress({ saveData, progress }) {
+    setProgress(progress + 100 / (28 / 5));
+    if (progress > 100) {
+      setProgress(100);
+    }
+    console.log(progress);
+  }
+  function MinusProgress({ saveData, progress }) {
+    setProgress(progress - 100 / (28 / 5));
+    if (progress < 0) {
+      setProgress(0);
+    }
+    console.log(progress);
+  }
   return (
     <div>
       <header>검사 진행</header>
       <h2>{progress}% 진행 중</h2>
       <div className="question">
-        {saveData.map((data) => {
+        {currentData.map((data) => {
           function inputHandler(e) {
             setUserAnswer(e.target.value);
             totalAnswer[Number(data.qitemNo) - 1] = userAnswer;
@@ -61,22 +81,8 @@ function TestPage() {
           );
         })}
       </div>
-      <button
-        onClick={() => {
-          setProgress(progress - 10);
-          console.log(progress);
-        }}
-      >
-        이전
-      </button>
-      <button
-        onClick={() => {
-          setProgress(progress + 10);
-          console.log(progress);
-        }}
-      >
-        다음
-      </button>
+      <button onClick={MinusProgress}>이전</button>
+      <button onClick={PlusProgress}>다음</button>
     </div>
   );
 }
