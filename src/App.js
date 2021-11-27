c11eb645385ce72a945497719dcab5937a6472eb
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-27 16:09:58 +0900

    11 27 8차 최종.. 스크롤투탑

--- src/App.js
+++ src/App.js
@@ -9,24 +9,27 @@ import styled from "styled-components";
 import { Route, Switch } from "react-router-dom";
 import UserInfo, { UserContext } from "./pages/UserInfo";
 import ResultGraph from "./pages/ResultGraph";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <UserInfo>
       <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <StartPage></StartPage>
-          </Route>
-          <Route path="/test_example">
-            <TestExample></TestExample>
-          </Route>
-          <Route path="/test">
-            <TestPage></TestPage>
-          </Route>
-          <Route path="/test_result" component={TestResultPage}></Route>
-          <Route path="/result_graph" component={ResultGraph}></Route>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/">
+              <StartPage></StartPage>
+            </Route>
+            <Route path="/test_example">
+              <TestExample></TestExample>
+            </Route>
+            <Route path="/test">
+              <TestPage></TestPage>
+            </Route>
+            <Route path="/test_result" component={TestResultPage}></Route>
+            <Route path="/result_graph" component={ResultGraph}></Route>
+          </Switch>
+        </ScrollToTop>
       </div>
     </UserInfo>
   );
